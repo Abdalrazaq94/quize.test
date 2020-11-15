3e32b4dd7154a3849321d9d7c940cab7bc5a23d3
Abd94 <abd.94t@gmail.com>
2020-11-15 19:05:09 +0000

    objects quiz

---
+++ index.js
@@ -0,0 +1,41 @@
+const quizDate = [
+    {
+        question: 'how old is ABD?',
+        a: '10',
+        b: '20',
+        c: '26',
+        d: '100',
+        correct: 'c'
+    }, 
+    {
+        question: 'what is the best progrraming language in 2020?',
+        a: 'Python',
+        b: 'C',
+        c: 'Javascript',
+        d: 'java ',
+        correct: 'c'
+    },
+    {
+        question: 'who is the presdinet of the USA?',
+        a: 'florina pop',
+        b: 'Abd',
+        c: 'Tramup',
+        d: 'June Piden ',
+        correct: 'd'
+    },
+    {
+        question: 'which year Covid 19 has started?',
+        a: '2020',
+        b: '2019',
+        c: '1999',
+        d: '1898 ',
+        correct: 'b'
+    },{
+        question: 'what is the biggist counrty in the worls',
+        a: 'Russia',
+        b: 'Amireca',
+        c: 'Canada',
+        d: ' Turkia',
+        correct: 'a'
+    }
+]
